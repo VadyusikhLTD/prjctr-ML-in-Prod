4c2a600e8d59dd18b6338ad6c96c56ce9ab35e3a
vadym <tug45ag@gmail.com>
2022-10-06 08:19:50 -0700

    add code test and fix model final activation layer error

--- week3/project-and-wandb/tests/test_models.py
+++ week3/project-and-wandb/tests/test_models.py
@@ -1,54 +1,23 @@
 import torch
-# from torch import nn
-# import torch.nn.functional as F
-# from tqdm import tqdm
+import torch.utils.data as data_utils
+from models import calc_accuracy, IdentityModel
 
 import pytest
 
-import image_classification.models.SimpleCNN as SimpleCNN
 
 @pytest.fixture
-def my_model():
-    return SimpleCNN()
+def my_identity_model() -> IdentityModel:
+    model = IdentityModel()
+    return model
 
 
-
-# class SimpleCNN(nn.Module):
-#     def __init__(self,
-#                  conv1channels_num=20,
-#                  conv2channels_num=20,
-#                  class_num=10,
-#                  use_bn_for_input=False,
-#                  use_bn_inside=True,
-#                  final_activation=None):
+@pytest.fixture
+def my_test_data():
+    train = data_utils.TensorDataset(torch.tensor([[.1, 0.9], [-0.5, 22], [0, 1]]), torch.tensor([1, 1, 0]) )
+    train_loader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
+    return train_loader
 
 
-
-# def calc_accuracy(test_data, model_, device):
-#     num_correct, num_samples = 0, 0
-#     model_.eval()
-#
-#     with torch.no_grad():
-#         data_tqdm = tqdm(test_data, desc="Accuracy calculation", ascii=True, miniters=len(test_data)//5)
-#         for x, y in data_tqdm:
-#             x = x.to(device)
-#             y = y.to(device)
-#
-#             pred_ = model_(x)
-#             _, pred_ = pred_.max(1)
-#             num_correct += (pred_ == y).sum()
-#             num_samples += pred_.size(0)
-#
-#     model_.train()
-#     return num_correct/num_samples
-
-
-if __name__ == "__main__":
-    model = SimpleCNN(
-        use_bn_for_input=True,
-                 use_bn_inside=True,
-                 final_activation='relu')
-    img = torch.randn(16, 1, 28, 28)
-    res = model(img)
-    print(model)
-    print(f"{img.shape} -> {res.shape}")
+def test_calc_accuracy(my_test_data, my_identity_model):
+    metrics = calc_accuracy(my_test_data, my_identity_model, "cpu")
+    assert abs(metrics - 2/3) < 1e-6
