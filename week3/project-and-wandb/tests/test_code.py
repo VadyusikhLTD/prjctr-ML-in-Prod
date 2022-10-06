b8e49b906cb655f6a95f600c56d6131c5bf30c04
vadym <tug45ag@gmail.com>
2022-10-06 15:43:53 -0700

    edit sources and write model tests

--- week3/project-and-wandb/tests/test_code.py
+++ week3/project-and-wandb/tests/test_code.py
@@ -1,21 +1,23 @@
 import torch
 import torch.utils.data as data_utils
-from models import SimpleCNN
+from models import calc_accuracy, IdentityModel
 
 import pytest
 
 
 @pytest.fixture
-def my_model() -> SimpleCNN:
-    model = SimpleCNN()
+def my_identity_model() -> IdentityModel:
+    model = IdentityModel()
     return model
 
 
 @pytest.fixture
 def my_test_data():
-    return torch.rand(16, 1, 28, 28)
+    train = data_utils.TensorDataset(torch.tensor([[.1, 0.9], [-0.5, 22], [0, 1]]), torch.tensor([1, 1, 0]))
+    train_loader = data_utils.DataLoader(train, batch_size=50, shuffle=True)
+    return train_loader
 
 
-def test_simple_predict(my_model, my_test_data):
-    result = my_model(my_test_data)
-    assert result.shape == torch.Size([16, 10])
+def test_calc_accuracy(my_test_data, my_identity_model):
+    metrics = calc_accuracy(my_test_data, my_identity_model, "cpu")
+    assert abs(metrics - 2/3) < 1e-6
